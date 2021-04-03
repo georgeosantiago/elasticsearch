780b8ee88bfe2e791981331e6ce9a9dcfc7fa9c1
Jorge Santiago <georgeosantiago@yahoo.com>
2021-04-03 16:04:19 -0300

    update

--- create_access.sh
+++ create_access.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+docker-compose up -d elasticsearch
+sleep 10
 docker-compose exec -T elasticsearch ./bin/elasticsearch-setup-passwords auto --batch > passwords.txt
+docker-compose down
+echo "Modificar kibana.yml"
